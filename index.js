7fac35989082c7abef966a9d825cf440cfc787f4
Dora <example@gmail.com>
2022-05-04 09:24:54 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const b = prompt ("Че делаешь?","Сижу");
+// console.log(b);
+
+const answer = [];
+
+answer[0] = prompt('Как ваше имя?','');
+answer[1] = prompt('Как ваша фамилия?','');
+answer[2] = prompt('Как ваше отчество?','');
+
+const zxc = prompt('Получается,ты гей?','Нет,я не гей');
+const a = prompt('Пиздишь?','нет');
+alert('Пиздишь');
+
+Document.write(answer);
